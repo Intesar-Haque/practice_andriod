cd01554342995038cddb392f5740dee0342f63f5
Fatema-Tuj-Johora <fatematujjohora3099@gmail.com>
2024-02-12 17:42:07 +0600

    Styling

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView iconImageView = findViewById(R.id.imageView4);
+        iconImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle icon click here
+                replaceSpaceWithNewFragment();
+            }
+        });
+    }
+
+    private void replaceSpaceWithNewFragment() {
+        // Create an instance of YourNewFragment
+        HomeFragment yourNewFragment = new HomeFragment();
+
+        // Replace the Space with YourNewFragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, yourNewFragment)
+                .addToBackStack(null) // Optional: Add the transaction to the back stack
+                .commit();
     }
 }
